ebdb0229997cc7314d675b3ded548e3a1d799949
jisanahamed09205 <jisanahamed09205@gmail.com>
2023-11-25 00:48:01 +0600

    navbar impliment

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <div>
                 <Outlet></Outlet>
             </div>
