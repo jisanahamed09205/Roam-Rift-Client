d5ca0926f9bf73143af4b7541b1af5f0a9ef4354
jisanahamed09205 <jisanahamed09205@gmail.com>
2024-02-11 23:53:54 +0600

    loader changed

--- src/Shared/Loader/Loader.jsx
+++ src/Shared/Loader/Loader.jsx
@@ -3,56 +3,6 @@ import './Loader.css'
 const Loader = () => {
     return (
         <>
-            {/* <div>
-                <div className="box-of-star1">
-                    <div className="star star-position1" />
-                    <div className="star star-position2" />
-                    <div className="star star-position3" />
-                    <div className="star star-position4" />
-                    <div className="star star-position5" />
-                    <div className="star star-position6" />
-                    <div className="star star-position7" />
-                </div>
-                <div className="box-of-star2">
-                    <div className="star star-position1" />
-                    <div className="star star-position2" />
-                    <div className="star star-position3" />
-                    <div className="star star-position4" />
-                    <div className="star star-position5" />
-                    <div className="star star-position6" />
-                    <div className="star star-position7" />
-                </div>
-                <div className="box-of-star3">
-                    <div className="star star-position1" />
-                    <div className="star star-position2" />
-                    <div className="star star-position3" />
-                    <div className="star star-position4" />
-                    <div className="star star-position5" />
-                    <div className="star star-position6" />
-                    <div className="star star-position7" />
-                </div>
-                <div className="box-of-star4">
-                    <div className="star star-position1" />
-                    <div className="star star-position2" />
-                    <div className="star star-position3" />
-                    <div className="star star-position4" />
-                    <div className="star star-position5" />
-                    <div className="star star-position6" />
-                    <div className="star star-position7" />
-                </div>
-                <div data-js="astro" className="astronaut">
-                    <div className="head" />
-                    <div className="arm arm-left" />
-                    <div className="arm arm-right" />
-                    <div className="body">
-                        <div className="panel" />
-                    </div>
-                    <div className="leg leg-left" />
-                    <div className="leg leg-right" />
-                    <div className="schoolbag" />
-                </div>
-            </div> */}
-            {/* Second */}
             <div className="spinner-container">
                 <div className="spinner"></div>
             </div>
